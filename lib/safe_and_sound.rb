2d9af82feaea240ebf6ab5dae5c6e7548e08bcd9
Axel Tetzlaff <axel.tetzlaff@shopify.com>
2022-04-04 21:41:54 -0400

    Add #define method to declare new types

--- lib/safe_and_sound.rb
+++ lib/safe_and_sound.rb
@@ -5,4 +5,11 @@ require 'safe_and_sound/version'
 ##
 # Namespace for the safe_and_sound classes
 module SafeAndSound
+  def self.define(type_name, *variants)
+    raise ArgumentError.new "type_name must be a Symbol" unless type_name.is_a?(Symbol)
+    new_type = Class.new
+    Object.const_set(type_name, new_type)
+
+    new_type
+  end
 end
--- test/safe_and_sound_test.rb
+++ test/safe_and_sound_test.rb
@@ -1,10 +1,25 @@
 # frozen_string_literal: true
 require 'test_helper'
 
-class SafeAndSoundTest < Minitest::Test
+module SafeAndSound
+  class SafeAndSoundTest < Minitest::Test
 
-  def test_that_kitty_can_eat
-    assert_equal "0.0.1", SafeAndSound::VERSION
+    def test_that_type_can_be_defined
+      type = SafeAndSound.define :MyType
+
+      assert_equal 'MyType', type.name
+      refute_nil MyType
+    end
+
+    def test_that_type_name_must_be_sym
+      assert_raises ArgumentError do
+        SafeAndSound.define 'MyType'
+      end
+    end
+
+    def test_that_type_can_be_defined_with_variants
+       SafeAndSound
+         .define(:MyTypeWithAField, AVariant: {})
+    end
   end
-
 end
